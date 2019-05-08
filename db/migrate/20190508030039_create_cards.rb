7637d01df8edeecdbfffdfa7fe34eeef6119a037
Rygel-XVI <micha232@umn.edu>
2019-05-07 20:02:19 -0700

    added initial migrations

---
+++ db/migrate/20190508030039_create_cards.rb
@@ -0,0 +1,12 @@
+class CreateCards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cards do |t|
+      t.string :location
+      t.string :suit
+      t.integer :number
+      t.integer :deck_id
+
+      t.timestamps
+    end
+  end
+end
