82f7c5c7c1ebb56a468bedf25e426456040c00ad
Rygel-XVI <micha232@umn.edu>
2019-05-07 20:16:40 -0700

    created deck creation for standard deck

--- app/controllers/api/deck_controller.rb
+++ app/controllers/api/deck_controller.rb
@@ -1,2 +1,16 @@
 class DeckController < ApplicationController
+
+  def index
+    render json: Deck.all
+  end
+
+  def create
+    deck = Deck.create!
+    deck.create_standard_deck
+    render json: deck
+  end
+
+  def destroy
+  end
+  
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,14 @@
 class Deck < ApplicationRecord
     has_many :cards
+
+    def create_standard_deck
+      suits = ["H","D","S","C"]
+      numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+      suits.each do |s|
+        numbers.each do |n|
+          self.cards << Card.create!(suit: s, number: n, deck_id: self.id)
+        end
+      end
+      self
+    end
 end
