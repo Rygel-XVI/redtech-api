f8ac7b3981ac6434eee2cc7c9e53ede145028d76
Rygel-XVI <micha232@umn.edu>
2019-05-09 20:55:30 -0700

    added updating cards through the game

--- app/controllers/api/card_controller.rb
+++ app/controllers/api/card_controller.rb
@@ -1,3 +1,19 @@
 class Api::CardController < ApplicationController
 
+  def update
+    set_card
+    @card.update(card_params)
+    render json: @card
+  end
+
+  private
+
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:location)
+  end
+
 end
